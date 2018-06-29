644c14514002ae2fe06942f14f0117edfdffb9f6
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2018-06-29 22:59:31 +0300

    added check auth middleware

---
+++ backend/middleware/check-auth.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    req.userData = decodedToken;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "You are not authenticated!" });
+  }
+};
